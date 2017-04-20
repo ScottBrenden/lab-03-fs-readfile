a6b35a48608ae653f646495f2a9a1781ae301bac
ScottBrenden <scott.b.brenden@gmail.com>
2017-04-19 17:53:36 -0700

    hex slices printing

--- lab-scott/lib/fs-module.js
+++ lab-scott/lib/fs-module.js
@@ -1,5 +1,26 @@
 'use strict';
 
+module.exports = exports = {};
+
 const fs = require('fs');
 
-fs.readfile(`${__dirname}/data/`)
+// exports.reads = function(){
+let hexArr = [];
+fs.readFile(`${__dirname}/../data/one.txt`, function(err, data) {
+  if(err) throw err;
+  hexArr.push(data.toString('hex',0,8));
+  console.log(data.toString('hex',0,8));
+});
+fs.readFile(`${__dirname}/../data/two.txt`, function(err, data){
+  if(err) throw err;
+  hexArr.push(data.toString('hex',0,8));
+  console.log(data.toString('hex',0,8));
+});
+fs.readFile(`${__dirname}/../data/three.txt`, function(err, data){
+  if(err) throw err;
+  hexArr.push(data.toString('hex',0,8));
+  console.log(data.toString('hex',0,8));
+});
+
+// };
+console.log(hexArr);
