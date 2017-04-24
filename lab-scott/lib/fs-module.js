4f68ebec703e519c2f97b0a2b16bfada7b319cf9
ScottBrenden <scott.b.brenden@gmail.com>
2017-04-24 11:35:34 -0700

    loop instead of three seperate readFiles

--- lab-scott/lib/fs-module.js
+++ lab-scott/lib/fs-module.js
@@ -6,21 +6,24 @@ const fs = require('fs');
 
 exports.hexArr = [];
 exports.reads = function(cb){
-  fs.readFile(`${__dirname}/../data/one.txt`, function(err, data) {
-    if(err) throw err;
-    exports.hexArr.push(data.toString('hex',0,8));
-    // console.log(`one ${data.toString('hex',0,8)}`);
-  });
+  let fileArr = ['one.txt', 'two.txt', 'three.txt'];
+  for (let i=0; i < fileArr.length; i++){
+    fs.readFile(`${__dirname}/../data/${fileArr[i]}`, function(err, data) {
+      if(err) throw err;
+      exports.hexArr.push(data.toString('hex',0,8));
+      // console.log(`${fileArr[i]}: ${data.toString('hex',0,8)}`);
+    });
+  }
 
-  fs.readFile(`${__dirname}/../data/two.txt`, function(err, data){
-    if(err) throw err;
-    exports.hexArr.push(data.toString('hex',0,8));
-    // console.log(`two ${data.toString('hex',0,8)}`);
-  });
-
-  fs.readFile(`${__dirname}/../data/three.txt`, function(err, data){
-    if(err) throw err;
-    exports.hexArr.push(data.toString('hex',0,8));
-    // console.log(`three ${data.toString('hex',0,8)}`);
-  });
+  // fs.readFile(`${__dirname}/../data/two.txt`, function(err, data){
+  //   if(err) throw err;
+  //   exports.hexArr.push(data.toString('hex',0,8));
+  //   // console.log(`two ${data.toString('hex',0,8)}`);
+  // });
+  //
+  // fs.readFile(`${__dirname}/../data/three.txt`, function(err, data){
+  //   if(err) throw err;
+  //   exports.hexArr.push(data.toString('hex',0,8));
+  //   // console.log(`three ${data.toString('hex',0,8)}`);
+  // });
 };
